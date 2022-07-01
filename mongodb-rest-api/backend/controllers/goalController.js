96b2225d97fb9bf6a7e7167b96ba304efc3d0946
Kmyrrpen <shanenescarimoro@gmail.com>
2022-07-01 16:09:29 +0800

    added models for mongoose and connected controller to DB

--- mongodb-rest-api/backend/controllers/goalController.js
+++ mongodb-rest-api/backend/controllers/goalController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const { default: mongoose } = require("mongoose");
+const Goal = require("../models/goalModel");
 
 /**
  * @desc Get Goals
@@ -6,7 +8,8 @@ const asyncHandler = require("express-async-handler");
  * @access Private
  */
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 /**
@@ -20,7 +23,10 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Invalid Body");
   }
 
-  res.status(201).json({ message: "set: GOAL" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(201).json(goal);
 });
 
 /**
@@ -29,7 +35,21 @@ const setGoal = asyncHandler(async (req, res) => {
  * @access Private
  */
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete: GOAL ${req.params.id}` });
+  // check if ID from params are valid
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400);
+    throw new Error("Goal not Found");
+  }
+
+  const goal = await Goal.findById(req.params.id);
+
+  // check if goal exists
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not Found!");
+  }
+  await goal.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 /**
@@ -38,7 +58,30 @@ const deleteGoal = asyncHandler(async (req, res) => {
  * @access Private
  */
 const putGoal = asyncHandler(async (req, res) => {
-  res.status(201).json({ message: `put: GOAL ${req.params.id}` });
+  // check if payload is valid
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Invalid Body");
+  }
+
+  // check if ID from params are valid
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400);
+    throw new Error("Goal not Found");
+  }
+
+  const goal = await Goal.findById(req.params.id);
+
+  // check if goal exists
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not Found!");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(201).json(updatedGoal);
 });
 
 module.exports = {
