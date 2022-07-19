b8b9b029ce28199b8c747830f0a767893e91e965
Kmyrrpen <shanenescarimoro@gmail.com>
2022-07-19 18:33:06 +0800

    Added frontend UI

--- mongodb-rest-api/frontend/src/App.tsx
+++ mongodb-rest-api/frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route element={<Dashboard />} path="/" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Register />} path="/register" />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ mongodb-rest-api/frontend/src/pages/Register.tsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
+const Register: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register{" "}
+        </h1>
+        <p>Please Create an Account</p>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Enter your name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Confirm password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Register;
